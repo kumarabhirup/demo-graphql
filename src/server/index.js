3fbac3d8649328f125112ad4d4b2a4144bd95cfd
Kumar Abhirup <kumar@iqubex.com>
2019-06-30 10:26:51 +0530

    📦 NEW: GraphiQL setup Also fixed .eslintrc

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,30 @@
 const express = require('express')
+const { buildSchema } = require('graphql')
+const expressGraphql = require('express-graphql')
+
 const sendFilePath = require('../lib/sendFilePath')
 
+// GraphQL schema
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`)
+// Root resolver
+const root = {
+  message: () => 'Hello World!',
+}
+
 const app = express()
+
+app.use(
+  '/graphql',
+  expressGraphql({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
 
 app.use('/public', express.static('src/public'))
 
